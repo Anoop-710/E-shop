c28ff1490861d97001f5ba858e959ff0bff772dc
anoop <anoopaathreya@gmail.com>
2023-12-28 09:46:24 +0530

    Auth & User API Slice

---
+++ frontend/src/slices/authSlice.js
@@ -0,0 +1,22 @@
+// This file is to set the user credentials to local storage and remove them
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  user: localStorage.getItem("userInfo")
+    ? JSON.parse(localStorage.getItem("userInfo"))
+    : null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setCredentials: (state, action) => {
+      state.userInfo = action.payload;
+      localStorage.setItem("userInfo", JSON.stringify(action.payload))``;
+    },
+  },
+});
+
+export const { setCredentials } = authSlice.actions;
+export default authSlice.reducer;
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "./slices/apiSlice";
 import cartSliceReducer from "./slices/cartSlice";
+import authSliceReducer from "./slices/authSlice"; // we don't bring cartSlice and productApiSlice here since those are children of apiSlice
+
 const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
     cart: cartSliceReducer,
+    auth: authSliceReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
