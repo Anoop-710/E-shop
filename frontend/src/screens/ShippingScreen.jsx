2bc43b1395abc433a3790e4848d9b5a68cdfce3b
anoop <anoopaathreya@gmail.com>
2023-12-29 16:57:32 +0530

    Private route for shipping and userInfo error fixed

--- frontend/src/screens/ShippingScreen.jsx
+++ frontend/src/screens/ShippingScreen.jsx
@@ -8,12 +8,12 @@ import { saveShippingAddress } from "../slices/cartSlice";
 const ShippingScreen = () => {
   const cart = useSelector((state) => state.cart);
   const { shippingAddress } = cart;
-  const [address, setAddress] = useState(shippingAddress?.address || "");
-  const [city, setCity] = useState(shippingAddress?.city || "");
+  const [address, setAddress] = useState(shippingAddress.address || "");
+  const [city, setCity] = useState(shippingAddress.city || "");
   const [postalCode, setPostalCode] = useState(
-    shippingAddress?.postalCode || ""
+    shippingAddress.postalCode || ""
   );
-  const [country, setCountry] = useState(shippingAddress?.country || "");
+  const [country, setCountry] = useState(shippingAddress.country || "");
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
