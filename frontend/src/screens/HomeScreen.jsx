8b1130cd0ec1a11ca548fc9f5079908e39e54565
anoop <anoopaathreya@gmail.com>
2024-01-02 20:35:38 +0530

    Paginate Component

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -5,6 +5,7 @@ import Product from "../components/Product";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
 import { useParams } from "react-router-dom";
+import Paginate from "../components/Paginate";
 const HomeScreen = () => {
   const { pageNumber } = useParams();
   const { data, isLoading, error } = useGetProductsQuery({ pageNumber });
@@ -26,6 +27,7 @@ const HomeScreen = () => {
               </Col>
             ))}
           </Row>
+          <Paginate pages={data.pages} page={data.page} />
         </>
       )}
     </>
