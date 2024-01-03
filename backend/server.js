131873332781ddf31d5cec6768411c55d7c4e66b
anoop <anoopaathreya@gmail.com>
2024-01-03 16:39:18 +0530

    build

--- backend/server.js
+++ backend/server.js
@@ -4,28 +4,21 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 dotenv.config();
 import connectDB from "./config/db.js";
-import products from "./data/products.js";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
-import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import uploadRoutes from "./routes/uploadRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
-connectDB(); // Connect to MongoDB
 const port = process.env.PORT || 5000;
+
+connectDB();
+
 const app = express();
 
-// Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-// cookie parser middleware
-app.use(cookieParser()); //allows us to access request.cookies, here jwt are our cookies
-
-// create a route
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
+app.use(cookieParser());
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
@@ -33,33 +26,28 @@ app.use("/api/orders", orderRoutes);
 app.use("/api/upload", uploadRoutes);
 
 app.get("/api/config/paypal", (req, res) =>
-  res.send({
-    clientId: process.env.PAYPAL_CLIENT_ID,
-  })
+  res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
 );
 
-// Make uploads folder static
-const __dirname = path.resolve(); //Set __dirname to the current directory
-app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use("/uploads", express.static("/var/data/uploads"));
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  const __dirname = path.resolve();
+  app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
 
-//Any time we hit the route 'api/products' it will go to productRoutes
-
-// get all products
-// app.get("/api/products", (req, res) => {
-//   res.json(products);
-// });
-
-// // get single product
-// app.get("/api/products/:id", (req, res) => {
-//   const product = products.find((p) => p._id === req.params.id);
-//   res.json(product);
-// });
-
-//Move the above product routes to productRoutes.js file and make use of express js instead
-// start the server
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+app.listen(port, () =>
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
+);
